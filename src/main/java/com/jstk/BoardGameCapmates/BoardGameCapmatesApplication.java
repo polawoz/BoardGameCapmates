555bc4727ab152484e6740a1e715c96be8316b2a
POWOZNIA <POWOZNIA@CE24244.corp.capgemini.com>
2018-07-23 09:51:17 +0200

    Empty project

---
+++ src/main/java/com/jstk/BoardGameCapmates/BoardGameCapmatesApplication.java
@@ -0,0 +1,12 @@
+package com.jstk.BoardGameCapmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameCapmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameCapmatesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jstk/BoardGameCapmates/BoardGameCapmatesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jstk.BoardGameCapmates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardGameCapmatesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
